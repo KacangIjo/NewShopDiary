1ce9af30a84205a9e4db82b3f77779c66f9124d4
Yeremia Priambodo <priambododo@outlook.com>
2018-06-30 10:01:21 +0700

    add model view model and services

---
+++ ShopDiaryApp/Helper/UrlHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ShopDiaryApp.Helper
+{
+    public class UrlHelper
+    {
+        private const string Domain_Url = "http://shopdiary.funcraftstudio.com/";
+        private const string Base_Url = Domain_Url + "api/";
+
+        #region Rests
+        public const string Locations_Url = Base_Url + "Locations";
+        public const string Storages_Url = Base_Url + "Storages";
+        public const string Inventories_Url = Base_Url + "Inventories";
+        public const string Inventorylogs_Url = Base_Url + "Inventorylogs";
+        public const string Categories_Url = Base_Url + "Categories";
+        public const string Products_Url = Base_Url + "Products";
+        public const string ShopList_Url = Base_Url + "ShopList";
+        public const string Consumes_Url = Base_Url + "Consumes";
+        public const string Purchases_Url = Base_Url + "Purchases";
+        public const string Shopitem_Url = Base_Url + "Shopitems";
+        public const string Shoplists_Url = Base_Url + "Shoplists";
+        public const string Userlocations_Url = Base_Url + "UserLocations";
+        public const string Rolelocations_Url = Base_Url + "RoleLocations";
+        #endregion
+
+        #region User
+        public const string Account_Url = Base_Url + "Account";
+        public const string Account_Login = Domain_Url + "Token";
+        
+        #endregion
+    }
+}
---
+++ ShopDiaryApp/Models/ViewModels/InventoryViewModel.cs
@@ -0,0 +1,55 @@
+﻿
+using ShopDiaryProject.Domain.Models;
+using System;
+
+namespace ShopDiaryApp.Models.ViewModels
+{
+    public class InventoryViewModel
+    {
+        public Guid Id { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string ItemName { get; set; }
+        public bool IsConsumed { get; set; }
+        public decimal Price { get; set; }
+        public string AddedUserId { get; set; }
+
+
+        public Guid ProductId { get; set; }
+        public Guid StorageId { get; set; }
+
+
+        public Inventory ToModel()
+        {
+            return new Inventory
+            {
+                Id = (Id == Guid.Empty) ? Guid.NewGuid() : Id,
+                ExpirationDate = ExpirationDate,
+                IsConsumed=IsConsumed,
+                Price=Price,
+                ProductId=ProductId,
+                StorageId=StorageId,
+                ItemName=ItemName,
+                AddedUserId=AddedUserId,
+            };
+        }
+        public InventoryViewModel()
+        {
+
+        }
+
+        public InventoryViewModel(Inventory i)
+        {
+            this.Id = i.Id;
+            this.ExpirationDate = i.ExpirationDate;
+            this.Price = i.Price;
+            this.ProductId = i.ProductId;
+            this.StorageId = i.StorageId;
+            this.ItemName = i.ItemName;
+            this.IsConsumed =i.IsConsumed;
+            this.AddedUserId = i.AddedUserId;
+
+
+        }
+       
+    }
+}
